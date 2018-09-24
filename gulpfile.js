88740789c1d117f85c06f180c3a6d62b257d7cfd
Rafael Mendoza <raffym0831@gmail.com>
2018-09-24 16:09:03 +0800

    Added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+
 var jsSources = [
 	'components/scripts/rclick.js',
 	'components/scripts/pixgrid.js',
@@ -12,16 +14,30 @@ var jsSources = [
 	'components/scripts/templates.js'
 ];
 
+var sassSources = ['components/sass/styles.scss'];
+
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
 		.pipe(coffee({bare: true})
 			.on('error', gutil.log))
-		.pipe(gulp.dest('components/scripts'))
+		.pipe(gulp.dest('components/scripts'));
 });
 
 gulp.task('js', function(){
 	gulp.src(jsSources)
 		.pipe(concat('scripts.js'))
 		.pipe(browserify())
-		.pipe(gulp.dest('builds/development/js'))
+		.pipe(gulp.dest('builds/development/js'));
+});
+
+gulp.task('compass', function(){
+	gulp.src(sassSources)
+		.pipe(compass({
+			css: 'builds/development/css',
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		})
+		.on('error', gutil.log))
+		.pipe(connect.reload());
 });
